b9ddc867ff1205973c4982e5611180832134274b
HannahYudkin <hannahyudkin@gmail.com>
2020-04-01 16:58:04 -0400

    Completed functionality.

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (name) VALUES ('Test1');
 INSERT INTO burgers (name) VALUES ('Test2');
-INSERT INTO burgers (name, sleepy) VALUES ('Test3', true);
-INSERT INTO burgers (name, sleepy) VALUES ('Test4', true);
+INSERT INTO burgers (name, eaten) VALUES ('Test3', true);
+INSERT INTO burgers (name, eaten) VALUES ('Test4', true);
 
