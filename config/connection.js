db683764afe5872167e72ef59cdf9f7acc2c0e4d
HannahYudkin <hannahyudkin@gmail.com>
2020-05-17 11:16:17 -0400

    Added changes for herokus deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,20 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root1234",
-  database: "burger_db"
-});
-
-// Make connection.
+require('dotenv').config()
+const mysql = require("mysql");
+// Set up connection information
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: 'localhost',
+    user: 'root',
+    dialect: 'mysql',
+    password: "root1234",
+    database: "burger_db"
+  });
+};
+// Connect to the database
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -17,6 +22,5 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-// Export connection for our ORM to use.
+// Export connection
 module.exports = connection;
